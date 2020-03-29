c0e2fc98f954c669602394247137dffadf4ebd2b
Kingjohnny <johnny@thinktomorrow.be>
2020-03-29 20:29:53 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Checklist</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="assets/main.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+  <main>
+  <article>Check item 1</article>
+  <article>Check item 2</article>
+  <article>Check item 3</article>
+  </main>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix  = require('laravel-mix');
+require('laravel-mix-purgecss');
+
+mix.js('resources/js/main.js', 'assets')
+    .sass('resources/css/main.scss', 'assets')
+    //.copy('resources/img', 'public/assets/front/img')
+    .setPublicPath('public')
+    .version()
+
+    .options({
+        // Our own set of PostCSS plugins.
+        postCss: [
+            require('tailwindcss')('./resources/css/tailwind.config.js'),
+        ],
+
+        autoprefixer: true,
+
+        // Webpack setting to ignore sass loader to follow url() paths
+        processCssUrls: false,
+    })
+
+    .purgeCss({
+        only: ['assets/main.css'],
+    });
