1909b561d77d1abe8435e607d075743a35e8b75b
Kingjohnny <johnny@thinktomorrow.be>
2020-03-31 08:30:04 +0200

    add celebrate tag and working counter

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Checklist
- 
+
+- [ ] Layout
+- [ ] de checklist wordt op dagelijkse basis gecleared zodat je op nieuw kan beginnen. (evt clear-knop)
+- [ ] Je krijgt een hig five als je ze allemaal gedaan hebt
+- [ ] De voortgang (0/4) moet nog gefixed worden
+- [ ] Werkend krijgen op een subsite van tt
+- [ ] Testen of met github actions we ook kunnen deployen naar de subsite 
+
--- public/index.php
+++ public/index.php
@@ -34,19 +34,26 @@
         <use xlink:href="#clipboard"></use>
       </svg>
       <h1 class="text-4xl">Jouw dagelijkse checklist</h1>
-      <p class="font-hairline mb-2"><span id="counter">0</span> van 4</p>
+      <p class="font-hairline mb-2"><span id="checkedItems">0</span> van <span id="numberOfItems">0</span></p>
       <p class="text-xl font-hairline">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut eaque error id asperiores! Dolores nihil nobis dignissimos repellendus eius odio necessitatibus obcaecati tempore esse, facilis blanditiis numquam accusamus totam soluta.</p>
     </div>
     <?php
     $listItemsWork = array(
-    'Neem korte pauzes om je benen te strekken',
-    'Stop op tijd met werken en ga een wandeling maken!',
-    'Drink voldoende water of thee',
-    'Eet gezond');
+      'Ik heb laten weten aan mijn collega\'s weten dat ik aanwezig ben',
+      'Ik heb een dagplanning gemaakt',
+      'Ik heb al mijn mails behandeld (max 1 uur)',
+      'Ik deed een stand-up met mijn team',
+      'Ik heb genoeg focus-time gehad vandaag',
+      'Ik heb mijn timetracking ingevuld.',
+      'Ik heb mijn meetings in de namiddag gepland.',
+      'Ik heb mijn collega\'s voldoende geïnformeerd over mijn taken',
+      'Ik heb minstens twee actieve activiteiten gedaan',
+      'Ik heb minstens 2 liter water gedronken.',
+    );
     ?>
 
     <?php foreach($listItemsWork as $a=>$item){ ?>
-      <article id="article-<?php echo $a; ?>" class="hover:line-through inset stack bg-primary-200 font-light cursor-pointer flex items-center justify-between" data-toggle-class="#article-<?php echo $a; ?>,checked|#icon-<?php echo $a; ?>">
+      <article id="article-<?php echo $a; ?>" class="checklistitems hover:line-through inset stack bg-primary-200 font-light cursor-pointer flex items-center justify-between" data-toggle-class="#article-<?php echo $a; ?>,active|#icon-<?php echo $a; ?>">
         <span><?php echo $item; ?></span>
         <svg id="icon-<?php echo $a; ?>" width="15" height="15" class="text-green-800 hidden fill-current">
           <use xlink:href="#check"></use>
@@ -54,7 +61,43 @@
       </article>
       <?php }; ?>
 
+      <div id="celebration" class="hidden fixed top-0 bottom-0 left-0 right-0 w-screen h-screen bg-black-50">
+        <img src="https://media1.giphy.com/media/10hO3rDNqqg2Xe/giphy.gif?cid=ecf05e47ad963626f79cccb46e2dc5b0ae5aa92bd5e20742&rid=giphy.gif" alt="celebrate" class="block mx-auto stack">
+      </div>
   </main>
+
+<script>
+  //get total of articles
+  var totalItems = document.querySelectorAll('article').length;
+  var totalNumber = document.getElementById("numberOfItems");
+  totalNumber.innerHTML = totalItems;
+
+
+  // TODO: show numbers of checked item in counter
+  var clickOnArticle = document.querySelectorAll('article');  
+  var numberOfItems = document.getElementById("checkedItems");
+  var totalChecked = document.getElementsByClassName('active').length;
+
+  function addCheckedItemsToDiv()
+  {
+    totalChecked ++;
+    numberOfItems.innerHTML = totalChecked;
+    if( totalChecked === totalItems){
+      timeToCelebrate();
+      // window.alert('Good job. Tijd om de deur achter je toe te trekken.')
+    }
+  };
+  for (i = 0; i < clickOnArticle.length; i++) {
+    clickOnArticle[i].addEventListener("click", addCheckedItemsToDiv);
+  }
+
+  function timeToCelebrate(){
+    var celebrate = document.getElementById("celebration");
+    celebrate.classList.remove("hidden");
+  }
+
+  
+</script>
 
   <footer class="container stack text-center font-hairline text-sm text-primary-800">
     &copy; 2020 - &#129412; &#127881; door Think Tomorrow
